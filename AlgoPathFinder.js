404f9fa275198669b7f8698915f5895dd784ee7a
AZ000ZA <150498639+az000za@users.noreply.github.com>
2023-12-19 19:13:47 -0500

    Update AlgoPathFinder.js

--- AlgoPathFinder.js
+++ AlgoPathFinder.js
@@ -43,48 +43,61 @@ const funcs = [
     ...funs_arith,
   ];
   
+  function TypePathFinder(inputType, outputType, funcs) {
+      const visited = {};
+      const paths = [];
+      function backtrack(currentType, currentPath) {
+        if (currentType === outputType) {
+          paths.push(currentPath.slice()); // Create a copy of the path
+          return;
+        }
+        for (const func of funcs) {
+          const input = func.input;
+          const output = func.output;
+          if (currentType === input && !visited[output]) {
+            visited[output] = true;
+            currentPath.push(func);
+            backtrack(output, currentPath);
+            currentPath.pop();
+            visited[output] = false;
+          }
+        }
+      }
+      backtrack(inputType, []);
+      return paths;
+    }
+  
+    function AlgoPathFinder(paths, funcs, input, goal, variations) {
+        if (variations === 0) {
+          return;
+        }
+        const queue = [[input, []]];
+        const visited = new Set();
+        while (queue.length) {
+          const [currentVal, currentPath] = queue.shift();
+          if (visited.has(currentVal)) {
+            continue;
+          }
+          visited.add(currentVal);
+          for (const func of funcs) {
+            const nextVal = func(currentVal);
+            const nextPath = [...currentPath, func.name, input];
+            if (nextVal === goal) {
+              paths.push(nextPath);
+              console.log("Path found:", nextPath);
+            } else if (!visited.has(nextVal)) {
+              queue.push([nextVal, nextPath]);
+            }
+          }
+        }
+        return paths;
+  }
+
   let input = 1;
   let output = input;
   let goal = 7;
   let paths = []; // Set of successful paths made to goal
   let intersections = {};
-  
-  function TypePathFinder(paths, funcs, input, goal, variations){
-        let inputType = input.type;
-        let goalType = goal.type;
-        let outputType = func.name;
-
-        
-      
-        if (func.name === goal) 
-        if (func.params.includes(input.type))
-  }
-  
-  function AlgoPathFinder(paths, funcs, input, goal, variations) {
-    if (variations === 0) {
-      return;
-    }
-    const queue = [[input, []]];
-    const visited = new Set();
-    while (queue.length) {
-      const [currentVal, currentPath] = queue.shift();
-      if (visited.has(currentVal)) {
-        continue;
-      }
-      visited.add(currentVal);
-      for (const func of funcs) {
-        const nextVal = func(currentVal);
-        const nextPath = [...currentPath, func.name, input];
-        if (nextVal === goal) {
-          paths.push(nextPath);
-          console.log("Path found:", nextPath);
-        } else if (!visited.has(nextVal)) {
-          queue.push([nextVal, nextPath]);
-        }
-      }
-    }
-    return paths;
-  }
   
   const foundPaths = AlgoPathFinder(paths, funcs, input, goal, 3);
   
