6c8c2eb9a462fa0b8f09d3419367ea3d61578961
AZ000ZA <150498639+az000za@users.noreply.github.com>
2023-12-19 21:52:01 -0500

    Update extractFunctionInfo.js

--- extractFunctionInfo.js
+++ extractFunctionInfo.js
@@ -1,43 +1,47 @@
-  function extractFunctionInfo(func, description = '', fileName = '') {
-    const functionString = func.toString();
-    const functionName = func.name || 'anonymous';
-    const functionParams = functionString.match(/\(([^)]*)\)/)[1].split(',').map(param => param.trim());
-    const functionBody = functionString.match(/\{([^]*)\}/)[1].trim();
+function extractFunctionInfo(func, description = '', fileName = '') {
+  const functionString = func.toString();
+  const functionName = func.name || 'anonymous';
+  const functionParams = functionString.slice(
+    functionString.indexOf('(') + 1,
+    functionString.indexOf(')')
+  ).split(',').map(param => param.trim());
+  const functionBody = functionString.slice(
+    functionString.indexOf('{') + 1,
+    functionString.lastIndexOf('}')
+  ).trim();
 
-    const parsedAST = acorn.parse(functionBody, { ecmaVersion: 'latest' });
-
-    const instructions = [];
-    acorn.walk.simple(parsedAST, {
-      FunctionDeclaration(node) {
-        const innerFunctionInfo = {
-          functionName: node.id.name,
-          paramIn: node.params.map(param => param.name),
-          instructions: [], // Placeholder for inner functions
-          paramOut: [], // Placeholder for output params of inner functions
-          fileName: fileName // Placeholder for file name
-        };
-        instructions.push(innerFunctionInfo);
-      }
+  const instructions = [];
+  const innerFunctions = functionBody.match(/function\s+([^\(]+)/g);
+  if (innerFunctions) {
+    innerFunctions.forEach(innerFunc => {
+      const innerFunctionName = innerFunc.replace('function', '').trim();
+      const innerFunctionInfo = {
+        functionName: innerFunctionName,
+        paramIn: [], // Placeholder for inner function's input parameters
+        instructions: [], // Placeholder for inner functions
+        paramOut: [], // Placeholder for output params of inner functions
+        fileName: fileName // Placeholder for file name
+      };
+      instructions.push(innerFunctionInfo);
     });
-
-    const functionInfo = {
-      functionName: functionName,
-      description: description,
-      paramIn: functionParams,
-      instructions: instructions.map(instruction => this.extractInnerFunction(instruction)),
-      paramOut: [], // Placeholder for output params of outer function
-      fileName: fileName // Placeholder for file name
-    };
-
-    const existingFuncIndex = this.functions.findIndex(func => func.functionName === functionName);
-    if (existingFuncIndex !== -1) {
-      this.functions[existingFuncIndex] = { ...this.functions[existingFuncIndex], ...functionInfo };
-    } else {
-      this.functions.push(functionInfo);
-    }
   }
 
-function extractInnerFunction(instruction) {
-  const func = new Function(`return ${instruction.functionName}`)();
-  return this.extractFunctionInfo(func);
+  function extractInnerFunction(instruction) {
+    const func = new Function(`return ${instruction.functionName}`)();
+    return this.extractFunctionInfo(func);
+  }
+
+  const functionInfo = {
+    functionName: functionName,
+    description: description,
+    paramIn: functionParams,
+    instructions: instructions.map(instruction => extractInnerFunction(instruction)),
+    paramOut: [], // Placeholder for output params of outer function
+    fileName: fileName // Placeholder for file name
+  };
+
+  // Placeholder for existingFuncIndex and this.functions management (to be defined elsewhere)
+  // If you have an array 'this.functions' to store the extracted function info, manage it accordingly here
+
+  return functionInfo;
 }
