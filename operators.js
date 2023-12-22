0e09441ed3df40a246ed8337be69eb7df8f416e9
AZ000ZA <150498639+az000za@users.noreply.github.com>
2023-12-22 11:51:30 -0500

    Update operators.js

--- operators.js
+++ operators.js
@@ -9,27 +9,27 @@
     // Exponential and Logarithmic
     { func: function(base, exponent) {base ** exponent}, name: 'Exponentiation' },
     { func: function(a){Math.pow(a, 2)}, name: 'Square' },
-    { func: function(a {Math.pow(a, 3)}, name: 'Cube' },
-    { func: function(a { Math.sqrt(a)}, name: 'Square Root' },
-    { func: function(a {Math.cbrt(a)}, name: 'Cube Root' },
-    { func: function(a {Math.log10(a)}, name: 'Logarithm (base 10)' },
-    { func: function(a {Math.log(a)}, name: 'Natural Logarithm' },
-    { func: function(a {Math.exp(a)}, name: 'Exponential Function' },
+    { func: function(a) {Math.pow(a, 3)}, name: 'Cube' },
+    { func: function(a) { Math.sqrt(a)}, name: 'Square Root' },
+    { func: function(a) {Math.cbrt(a)}, name: 'Cube Root' },
+    { func: function(a) {Math.log10(a)}, name: 'Logarithm (base 10)' },
+    { func: function(a) {Math.log(a)}, name: 'Natural Logarithm' },
+    { func: function(a) {Math.exp(a)}, name: 'Exponential Function' },
   
     // Trigonometric Functions
-    { func: function(a {Math.sin(a)}, name: 'Sine' },
-    { func: function(a {Math.cos(a)}, name: 'Cosine' },
-    { func: function(a {Math.tan(a)}, name: 'Tangent' },
-    { func: function(a {Math.asin(a)}, name: 'Arcsine' },
-    { func: function(a {Math.acos(a)}, name: 'Arccosine' },
-    { func: function(a {Math.atan(a)}, name: 'Arctangent' },
+    { func: function(a) {Math.sin(a)}, name: 'Sine' },
+    { func: function(a) {Math.cos(a)}, name: 'Cosine' },
+    { func: function(a) {Math.tan(a)}, name: 'Tangent' },
+    { func: function(a) {Math.asin(a)}, name: 'Arcsine' },
+    { func: function(a) {Math.acos(a)}, name: 'Arccosine' },
+    { func: function(a) {Math.atan(a)}, name: 'Arctangent' },
   
     // Rounding and Absolute Value
-    { func: function(a {Math.abs(a)}, name: 'Absolute Value' },
-    { func: function(a {Math.ceil(a)}, name: 'Ceiling' },
-    { func: function(a {Math.floor(a)}, name: 'Floor' },
-    { func: function(a {Math.round(a)}, name: 'Round' },
-    { func: function(a {Math.trunc(a)}, name: 'Truncate (Remove Decimal)' },
+    { func: function(a) {Math.abs(a)}, name: 'Absolute Value' },
+    { func: function(a) {Math.ceil(a)}, name: 'Ceiling' },
+    { func: function(a) {Math.floor(a)}, name: 'Floor' },
+    { func: function(a) {Math.round(a)}, name: 'Round' },
+    { func: function(a) {Math.trunc(a)}, name: 'Truncate (Remove Decimal)' },
   
     // Minimum and Maximum
     { func: function(a, b) {Math.max(a, b)}, name: 'Maximum' },
@@ -139,7 +139,7 @@
 
   // Bitwise NOT
   {
-    func: function(a { ~a},
+    func: function(a) { ~a},
     name: 'bitwiseNOT',
   },
 
